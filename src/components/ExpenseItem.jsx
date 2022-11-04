c1f367c7f3d028224d0fecc3a5f03add61c6a7d0
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-11-04 14:20:41 +0530

    const month=props.date.toLocaleString('en-US',{month:'long'});

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props){
-    const expenseDate=new Date(2021,2,28);
-    const expenseTitle='Car Isurance';
-    const expenseAmount=294.67;
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const year=props.date.getFullYear();
     return(
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
