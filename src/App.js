0602fa60788492ff0a0646312d36fe73eb2a4187
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-11-05 09:58:14 +0530

    Organizing react components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 const expenses=[
   {
@@ -29,14 +29,10 @@ const expenses=[
 ]
 
 function App() {
-  return (
+  return (    
     <div>
       <h2>let's started react</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-
+      <Expenses items={expenses}/>
     </div>
   );
 }
